53f36068907cb2f8820bb71294c86b4898ea4ba2
dean <deanm1278@gmail.com>
2018-10-29 13:24:53 -0400

    DM: fix GD25Q

--- Adafruit_QSPI_GD25Q.cpp
+++ Adafruit_QSPI_GD25Q.cpp
@@ -9,21 +9,24 @@
 #include "Adafruit_QSPI_GD25Q.h"
 
 enum {
-	GD25Q_READ_STATUS = 0,
-	GD25Q_READ_STATUS1,
+	GD25Q_READ_STATUS1 = 0,
+	GD25Q_READ_STATUS2,
 	GD25Q_WRITE_ENABLE_VOLATILE_STATUS,
 	GD25Q_READ_IDS,
+	GD25Q_WRITE_STATUS2,
 };
 
 static const QSPIInstr cmdSetGD25Q[] = {
 		//read status
 		{ 0x05, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN | QSPI_INSTRFRAME_DATAEN), QSPI_READ, 0 },
-		//read status1
+		//read status2
 		{ 0x35, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN | QSPI_INSTRFRAME_DATAEN), QSPI_READ, 0 },
 		//write enable volatile status
-		{ 0x50, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN), QSPI_WRITE, 0 },
+		{ 0x50, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN), QSPI_READ, 0 },
 		//read ids
 		{ 0x9F, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN | QSPI_INSTRFRAME_DATAEN), QSPI_READ, 0 },
+		//write status2
+		{ 0x31, false, QSPI_ADDRLEN_NONE, QSPI_OPCODE_LEN_NONE, QSPI_IO_FORMAT_SINGLE, (QSPI_OPTION_INSTREN | QSPI_OPTION_DATAEN), QSPI_WRITE, 0 },
 };
 
 /**************************************************************************/
@@ -48,22 +51,22 @@ bool Adafruit_QSPI_GD25Q::begin()
 
 	writeStatus();
 
-	QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS], 0, NULL, ((uint8_t *)&_status.reg), 1);
+	QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS1], 0, NULL, ((uint8_t *)&_status.reg), 1);
 	while(_status.bit.WIP){
 		delay(1);
-		QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS], 0, NULL, ((uint8_t *)&_status.reg), 1);
+		QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS1], 0, NULL, ((uint8_t *)&_status.reg), 1);
 	}
 
-	QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS1], 0, NULL, ((uint8_t *)&_status.reg) + 1, 1);
+	QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_READ_STATUS2], 0, NULL, ((uint8_t *)&_status.reg) + 1, 1);
 	return (_status.bit.QE);
 }
 
 void Adafruit_QSPI_GD25Q::writeStatus()
 {
-	uint8_t c[] = {(uint8_t)(_status.reg & 0xFF), (uint8_t)((_status.reg >> 8) & 0xFF)};
+	uint8_t c = (uint8_t)((_status.reg >> 8) & 0xFF);
 
 	QSPI0.runInstruction(&cmdSetGeneric[ADAFRUIT_QSPI_GENERIC_CMD_WRITE_ENABLE]);
 
-	QSPI0.runInstruction(&cmdSetGeneric[ADAFRUIT_QSPI_GENERIC_CMD_WRITE_STATUS], 0, c, NULL, 2);
+	QSPI0.runInstruction(&cmdSetGD25Q[GD25Q_WRITE_STATUS2], 0, &c, NULL, 1);
 }
 
