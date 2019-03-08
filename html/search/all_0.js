ab1a33b02328adb23235c5615bc54c36c526c428
Travis CI <travis@travis-ci.org>
2019-03-08 18:13:47 +0000

    Deploy code docs to GitHub Pages Travis build: 32
    Commit: 52241b147c84f1fe712541f178127c38ced12948

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -8,6 +8,5 @@ var searchData=
   ['adafruit_5fqspi_5fs25fl1',['Adafruit_QSPI_S25FL1',['../class_adafruit___q_s_p_i___s25_f_l1.html',1,'']]],
   ['addrlen',['addrLen',['../struct_q_s_p_i_instr.html#aaa9969b9f273ba7b83e6f3b9b48ae351',1,'QSPIInstr']]],
   ['attachinterrupt',['attachInterrupt',['../class_adafruit___q_s_p_i.html#a31c0087fa61f41eb5b83fa80e322689f',1,'Adafruit_QSPI']]],
-  ['adafruit_20qspi_20library',['Adafruit QSPI library',['../index.html',1,'']]],
-  ['adafruit_20qspi_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fqspi_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fqspi_29',['Adafruit QSPI Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_QSPI.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_QSPI)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20qspi_20library',['Adafruit QSPI library',['../index.html',1,'']]]
 ];
